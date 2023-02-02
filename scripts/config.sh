201a21889e0b279d00754e4fc289f3a7e3f97789
ca <39785634+chenang1994@users.noreply.github.com>
2023-02-02 09:34:52 +0800

    Update config.sh

--- scripts/config.sh
+++ scripts/config.sh
@@ -5,7 +5,9 @@ sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
 #config1
 sed -i 's/CONFIG_TARGET_KERNEL_PARTSIZE=32/CONFIG_TARGET_KERNEL_PARTSIZE=64/g' configs/rockchip/01-nanopi
-sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=512/CONFIG_TARGET_ROOTFS_PARTSIZE=1024/g' configs/rockchip/01-nanopi
+sed -i 's/CONFIG_PACKAGE_luci-theme-material=y/CONFIG_PACKAGE_luci-theme-material=n/g' configs/rockchip/01-nanopi
+sed -i 's/CONFIG_PACKAGE_luci-theme-openwrt=y/CONFIG_PACKAGE_luci-theme-openwrt=n/g' configs/rockchip/01-nanopi
+sed -i 's/CONFIG_PACKAGE_luci-theme-openwrt-2020=y/CONFIG_PACKAGE_luci-theme-openwrt-2020=n/g' configs/rockchip/01-nanopi
 #config2
 echo 'CONFIG_PACKAGE_luci-app-adbyby-plus=y' >> configs/rockchip/01-nanopi
 echo 'CONFIG_PACKAGE_luci-app-qbittorrent=y' >> configs/rockchip/01-nanopi
